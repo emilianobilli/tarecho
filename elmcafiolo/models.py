6e1ccbac942579d60285ae950d04fb992642c1ed
Nicolas Pajoni <npajoni@claxson.com>
2016-08-10 17:44:52 -0300

    cambios en elmcafiolo getBestTranscoder y agregado de transcoder por preset.

--- elmcafiolo/models.py
+++ elmcafiolo/models.py
@@ -5,49 +5,48 @@ from django.db import models
 # Create your models here.
 
 
-class Preset(models.Model):
-    name = models.CharField(max_length=50, unique=True, help_text="Name of the Preset")
-
-    def __unicode__(self):
-	return self.name
-
-
 class Transcoder(models.Model):
-    name	   = models.CharField(max_length=50, unique=True, help_text="Name of the Transcoder")
-    ip   	   = models.CharField(max_length=15, help_text="IP of the Transcoder")
-    port 	   = models.IntegerField(help_text="Port of the Transcoder")
-    slots	   = models.IntegerField(blank=True, null=True, help_text="Availables slots")
-    message	   = models.CharField(max_length=510, blank=True,  help_text="Error or Warning message")
-    preset_master  = models.BooleanField()
-    enabled	   = models.BooleanField()
+    name            = models.CharField(max_length=50, unique=True, help_text="Name of the Transcoder")
+    ip              = models.CharField(max_length=15, help_text="IP of the Transcoder")
+    port            = models.IntegerField(help_text="Port of the Transcoder")
+    slots           = models.IntegerField(blank=True, null=True, help_text="Availables slots")
+    message         = models.CharField(max_length=510, blank=True,  help_text="Error or Warning message")
+    preset_master   = models.BooleanField()
+    enabled         = models.BooleanField()
     
     def __unicode__(self):
-	return self.name
+        return self.name
 
+
+class Preset(models.Model):
+    name        = models.CharField(max_length=50, unique=True, help_text="Name of the Preset")
+    transcoder  = models.ManyToManyField(Transcoder)
+
+    def __unicode__(self):
+        return self.name
 
 
 class Job(models.Model):
     STATUS = (
-	('U', 'Unassigned'),
-        ('Q', 'Queued'),
-        ('P', 'Processing'),
-        ('D', 'Done'),
-        ('E', 'Error')
+                ('U', 'Unassigned'),
+                ('Q', 'Queued'),
+                ('P', 'Processing'),
+                ('D', 'Done'),
+                ('E', 'Error')
     )
-    name		= models.CharField(max_length=50, help_text="Name of the Job")
-    input_filename	= models.CharField(max_length=255, help_text="Input File")
-    input_path		= models.CharField(max_length=255, help_text="Input Path")
-    basename          	= models.CharField(max_length=100, help_text="Destination Basename")
-    output_path		= models.CharField(max_length=255, help_text="Destination Path")
-    preset		= models.ForeignKey(Preset)
-    priority          	= models.CharField(max_length=1, default='5', help_text="0: Max priority, 9: Min Priority")
-    job_id		= models.IntegerField(blank=True, null=True, help_text="Transcoder job ID")
-    transcoder		= models.ForeignKey(Transcoder, blank=True, null=True)
-    status		= models.CharField(max_length=1, choices=STATUS, default='U', help_text="Job Status")
-    progress          	= models.IntegerField(default=0, help_text="Job Progress")
-    speed		= models.CharField(max_length=10, blank=True)
-    message           	= models.CharField(max_length=510, blank=True,  help_text="Error or Warning message")
+    name            = models.CharField(max_length=50, help_text="Name of the Job")
+    input_filename  = models.CharField(max_length=255, help_text="Input File")
+    input_path      = models.CharField(max_length=255, help_text="Input Path")
+    basename        = models.CharField(max_length=100, help_text="Destination Basename")
+    output_path     = models.CharField(max_length=255, help_text="Destination Path")
+    preset          = models.ForeignKey(Preset)
+    priority        = models.CharField(max_length=1, default='5', help_text="0: Max priority, 9: Min Priority")
+    job_id          = models.IntegerField(blank=True, null=True, help_text="Transcoder job ID")
+    transcoder      = models.ForeignKey(Transcoder, blank=True, null=True)
+    status          = models.CharField(max_length=1, choices=STATUS, default='U', help_text="Job Status")
+    progress        = models.IntegerField(default=0, help_text="Job Progress")
+    speed           = models.CharField(max_length=10, blank=True)
+    message         = models.CharField(max_length=510, blank=True,  help_text="Error or Warning message")
 
     def __unicode__(self):
-	return self.name
-    
+        return self.name
--- elmcafiolo_daemon.py
+++ elmcafiolo_daemon.py
@@ -39,35 +39,44 @@ PID_FILE = './pid/elmcafiolo.pid'
 def initTranscoders():
     enabledList = Transcoder.objects.filter(enabled = True)
     for trans in enabledList:
-	try:
-	    elm = elmServer(trans.ip, trans.port)
-	    trans.slots = elm.workers()
-	    trans.save()
-	except elmError as e:
-	    trans.enabled = False
-	    trans.message = e
-	    trans.save()
-	    logging.error('elmServer(): Error %s' % e)
+        try:
+            elm = elmServer(trans.ip, trans.port)
+            trans.slots = elm.workers()
+            trans.save()
+        except elmError as e:
+            trans.enabled = False
+            trans.message = e
+            trans.save()
+            logging.error('elmServer(): Error %s' % e)
 
 
 def getTranscodersSlots():
     enabledList = Transcoder.objects.filter(enabled = True)
     slots = 0
     for trans in enabledList:
-	slots = slots + trans.slots
+        slots = slots + trans.slots
     return slots 
 
 
-def getBestTranscoder():
+def getBestTranscoder(preset):
+    presetTrans = preset.transcoder.all()
     enabledList = Transcoder.objects.filter(enabled = True)
+    transList = []
+
+    for trans in enabledList:
+        if trans in presetTrans:
+            transList.append(trans)
+
     bestTrans = None
     bestTransFreeSlots = 0
-    for trans in enabledList:
-	activeJobs = len(Job.objects.filter(transcoder = trans).filter(Q(status = 'Q') | Q(status = 'P')))
-	freeSlots = trans.slots - activeJobs
-	if freeSlots > bestTransFreeSlots:
-	    bestTransFreeSlots = freeSlots
-	    bestTrans = trans
+
+    for trans in transList:
+        activeJobs = len(Job.objects.filter(transcoder = trans).filter(Q(status = 'Q') | Q(status = 'P')))
+        freeSlots = trans.slots - activeJobs
+        if freeSlots > bestTransFreeSlots:
+            bestTransFreeSlots = freeSlots
+            bestTrans = trans
+
     return bestTrans
 
 
@@ -79,34 +88,34 @@ def getScheduleableJob():
     jobs = []    
 
     if max_slots - used_slots > 0:
-	jobs = Job.objects.filter(status='U').order_by('-priority')
+        jobs = Job.objects.filter(status='U').order_by('-priority')
 
     if len(jobs) > 0:
-	return jobs[0]
+        return jobs[0]
 
 
 
 def startJob(job, transcoder):
     try:
-	elm = elmServer(transcoder.ip, transcoder.port)
-	presetId, presetType = elm.preset(job.preset.name)
+        elm = elmServer(transcoder.ip, transcoder.port)
+        presetId, presetType = elm.preset(job.preset.name)
     except elmError as e:
-	transcoder.enable   = False
-	transcoder.message  = e
-	transcoder.save()
-	# agregar logueo
-	return None
+        transcoder.enable   = False
+        transcoder.message  = e
+        transcoder.save()
+        # agregar logueo
+        return None
 
     if presetId is None:
-	job.status = 'E'
-	job.message = 'Preset ID not found'
-	job.save()
-	return None
+        job.status = 'E'
+        job.message = 'Preset ID not found'
+        job.save()
+        return None
     if presetType is None:
-	job.status = 'E'
-	job.message = 'Preset type not found'
-	job.save()
-	return None
+        job.status = 'E'
+        job.message = 'Preset type not found'
+        job.save()
+        return None
 
     jobElm = elmJob(elm)
     jobElm.name               = job.name
@@ -114,43 +123,41 @@ def startJob(job, transcoder):
     if presetType == 'hls':
         jobElm.hls_preset_id  = presetId
     if presetType == 'h264':
-	jobElm.h264_preset_id = presetId
+        jobElm.h264_preset_id = presetId
     jobElm.input_path         = job.input_path
     jobElm.basename           = job.basename
     jobElm.system_path        = False
     jobElm.output_path        = job.output_path
 
     try:
-	job.job_id = jobElm.start()
-	job.transcoder = transcoder
-	job.status = 'Q'
-	job.save()
+        job.job_id = jobElm.start()
+        job.transcoder = transcoder
+        job.status = 'Q'
+        job.save()
     except elmError as e:
-	job.message = 'Could not send job to transcoder ' + transcoder
-	transcoder.enabled = False
-	transcoder.save()
-	return None
+        job.message = 'Could not send job to transcoder ' + transcoder
+        transcoder.enabled = False
+        transcoder.save()
+        return None
 
     return job.job_id
     
-	
 
 def updateJobStatus(job):
     try:
-	elm = elmServer(job.transcoder.ip, job.transcoder.port)
+        elm = elmServer(job.transcoder.ip, job.transcoder.port)
     except elmError as e:
-	job.status = 'E'
-	job.message = 'Transcoder ' + transcoder + 'unavailable'
-	job.save()
-	transcoder.enabled = False
-	transcoder.save()
+        job.status = 'E'
+        job.message = 'Transcoder ' + transcoder + 'unavailable'
+        job.save()
+        transcoder.enabled = False
+        transcoder.save()
     
     jobElm = elmJob(elm, job.job_id)
     jobStatus = jobElm.status()
     job.status  = jobStatus
     job.message = jobElm.message()
     job.save()
-
 
 
 def elmCafioloMain():
@@ -162,24 +169,23 @@ def elmCafioloMain():
     initTranscoders()
     
     while True:
-	
-	job = getScheduleableJob()
-	
-	while job is not None:
-    	
-	    transcoder = getBestTranscoder()
 
-	    if transcoder is not None:
-		startJob(job, transcoder)
-	    else:
-		break
-	    job = getScheduleableJob()
+        job = getScheduleableJob()
 
-	job_list = Job.objects.filter(Q(status = 'Q') | Q(status = 'P'))
-	for job in job_list:
-	    updateJobStatus(job)
+        while job is not None:
+            transcoder = getBestTranscoder(job.preset)
 
-	time.sleep(30)
+            if transcoder is not None:
+                startJob(job, transcoder)
+            else:
+                break
+            job = getScheduleableJob()
+
+        job_list = Job.objects.filter(Q(status = 'Q') | Q(status = 'P'))
+        for job in job_list:
+            updateJobStatus(job)
+
+        time.sleep(30)
 
 
 
